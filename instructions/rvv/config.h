5a85865dc1d26fcbd3f1ccb848d688c963af60dc
Olaf Bernstein <camel-cdr@protonmail.com>
2026-01-17 12:01:55 +0100

    fix instructions/rvv fp

--- instructions/rvv/config.h
+++ instructions/rvv/config.h
@@ -21,30 +21,32 @@
 #endif
 
 #if __riscv_v_elen_fp >= 64
-#define T_F   (T_FH & 0b1110111011101110111011101110) // float
+#define T_F   (T_FH | 0b1100110011001100110011001100) // float
+#define T_Fw  (T_F & (T_F >> 1))
 #else
-#define T_F   (T_FH & 0b0110011001100110011001100110) // float
+#define T_F   (T_FH | 0b0100010001000100010001000100) // float
+#define T_Fw  (T_F & (T_F >> 1))
 #endif
 
 //                      m8  m4  m2  m1  mf2 mf4 mf8
-//                 SEW: 6310    6310    6310    6310
-//                      4268... 4268... 4268... 4268
-#define T_W    (T_A & 0b0000011101110111011101110111) // widen
-#define T_WR   (T_A & 0b0111011101110111011101110111) // widen reduction
-#define T_N    (T_A & 0b0000011101110111011101110111) // narrow
-#define T_FW   (T_F & 0b0000011001100110011001100110) // float widen
-#define T_FWR  (T_F & 0b0110011001100110011001100110) // float widen reduction
-#define T_FN   (T_F & 0b0000011001100110011001100110) // float narrow
+//                  SEW: 6310    6310    6310    6310
+//                       4268... 4268... 4268... 4268
+#define T_W    (T_A  & 0b0000011101110111011101110111) // widen
+#define T_WR   (T_A  & 0b0111011101110111011101110111) // widen reduction
+#define T_N    (T_A  & 0b0000011101110111011101110111) // narrow
+#define T_FW   (T_Fw & 0b0000011001100110011001100110) // float widen
+#define T_FWR  (T_Fw & 0b0110011001100110011001100110) // float widen reduction
+#define T_FN   (T_Fw & 0b0000011001100110011001100110) // float narrow
 
-#define T_VF2  (T_A & 0b1110111011101110111011101110) // extend 2
-#define T_VF4  (T_A & 0b1100110011001100110011001100) // extend 4
-#define T_VF8  (T_A & 0b1000100010001000100010001000) // extend 8
-#define T_ei16 (T_A & 0b1110111111111111111111111111) // no e8m8
+#define T_VF2  (T_A  & 0b1110111011101110111011101110) // extend 2
+#define T_VF4  (T_A  & 0b1100110011001100110011001100) // extend 4
+#define T_VF8  (T_A  & 0b1000100010001000100010001000) // extend 8
+#define T_ei16 (T_A  & 0b1110111111111111111111111111) // no e8m8
 
-#define T_E8   (T_A & 0b1111111111111111011100110001) // EEW=8
-#define T_E16  (T_A & 0b1110111011101110111111111111) // EEW=16
-#define T_E32  (T_A & 0b1100110011001100111011111111) // EEW=32
-#define T_E64  (T_A & 0b1000100010001000110011101111) // EEW=64
+#define T_E8   (T_A  & 0b1111111111111111011100110001) // EEW=8
+#define T_E16  (T_A  & 0b1110111011101110111111111111) // EEW=16
+#define T_E32  (T_A  & 0b1100110011001100111011111111) // EEW=32
+#define T_E64  (T_A  & 0b1000100010001000110011101111) // EEW=64
 
 #define T_64   0b1000100010001000100000000000 // only SEW=64
 #define T_GSH  0b0100010001000100000000000000 // gash
